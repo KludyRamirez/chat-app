34faf0b4a1daca61fbfb996ecd58252b0cd9d2fc
Kludy Ramirez <ramirezkludy23@gmail.com>
2024-09-11 14:17:46 +0800

    add initial middlewares: BA-1

---
+++ server/middlewares/VerifyCSRF.js
@@ -0,0 +1,14 @@
+const csurf = require('csurf');
+
+const csrfToken = csurf({ cookie: true });
+
+const csrfErrorHandler = (err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  res.status(403).send('Invalid CSRF token');
+};
+
+module.exports = {
+  csrfToken,
+  csrfErrorHandler,
+};
